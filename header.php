b378f646040439505dfd2ae414c1bc6690a301a3
Іванків Вадим <vivankiv@gmail.com>
2026-05-09 21:39:26 +0300

    Tighten shared header and footer

--- header.php
+++ header.php
@@ -5,6 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 require_once __DIR__ . '/include/functions.php';
 $menus = get_menu();
 $currentPage = basename($_SERVER['PHP_SELF']);
+$currentCategoryId = isset($_GET['category_id']) ? (int)$_GET['category_id'] : 0;
 ?>
 <!doctype html>
 <html lang="uk">
@@ -26,16 +27,16 @@ $currentPage = basename($_SERVER['PHP_SELF']);
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item <?= $currentPage === 'index.php' ? 'active' : '' ?>">
-                    <a class="nav-link" href="index.php">Головна</a>
+                    <a class="nav-link" href="index.php" <?= $currentPage === 'index.php' ? 'aria-current="page"' : '' ?>>Головна</a>
                 </li>
                 <?php foreach ($menus as $menu): ?>
-                    <li class="nav-item <?= isset($_GET['category_id']) && (int)$_GET['category_id'] === (int)$menu['id'] ? 'active' : '' ?>">
-                        <a class="nav-link" href="category.php?category_id=<?= e($menu['id']); ?>"><?= e($menu['name']); ?></a>
+                    <li class="nav-item <?= $currentCategoryId === (int)$menu['id'] ? 'active' : '' ?>">
+                        <a class="nav-link" href="category.php?category_id=<?= e($menu['id']); ?>" <?= $currentCategoryId === (int)$menu['id'] ? 'aria-current="page"' : '' ?>><?= e($menu['name']); ?></a>
                     </li>
                 <?php endforeach; ?>
             </ul>
             <form class="form-inline my-2 my-lg-0" action="search.php" method="get">
-                <input class="form-control mr-sm-2" type="search" name="q" placeholder="Пошук" value="<?= isset($_GET['q']) ? e($_GET['q']) : '' ?>">
+                <input class="form-control mr-sm-2" type="search" name="q" placeholder="Пошук по архіву" value="<?= isset($_GET['q']) ? e($_GET['q']) : '' ?>">
                 <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Знайти</button>
             </form>
             <a class="btn btn-warning ml-lg-3" href="login/index.php">Адмін</a>
